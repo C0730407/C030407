7d779d669956544e2d894c570616820b48a62d29
visualstudio <visualstudio@230-PC06>
2019-03-14 13:32:34 -0400

    starter code

---
+++ 730407--731777/730407--731777/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _730407__731777
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
